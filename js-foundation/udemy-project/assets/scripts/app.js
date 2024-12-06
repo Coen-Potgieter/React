dd2c5597da94b694cbb9e06d89667de26e21d83f
Gerhardus Coenraad Potgieter <25999656@sun.ac.za>
2024-12-06 10:30:49 +0200

    Finished up javascript refresher section, will now start with React

--- js-foundation/udemy-project/assets/scripts/app.js
+++ js-foundation/udemy-project/assets/scripts/app.js
@@ -96,7 +96,7 @@
 // console.log(eclecticArray[2]);
 // console.log(eclecticArray[3]);
 
-// Playing with .findIndex()
+// // `.findIndex()`
 // const myWords = ["ball", "yoyo", "toothpaste"];
 
 // const yoyoIdx = myWords.findIndex(item => item === "yoyo");
@@ -121,30 +121,187 @@
 //     console.log(num);
 // });
 
-// Playing with .map()
+// // `.map()`
+// const randomNums = [1, 78, 90, 1, 901, 1000];
+// const sqauredRandomNums = randomNums.map(num => num*num);
+// console.log(sqauredRandomNums);
 
-const randomNums = [1, 78, 90, 1, 901, 1000];
-const sqauredRandomNums = randomNums.map(num => num*num);
-console.log(sqauredRandomNums);
+// const bunchOfHellos = randomNums.map(() => "Hello");
+// console.log(bunchOfHellos);
 
-const bunchOfHellos = randomNums.map(() => "Hello");
-console.log(bunchOfHellos);
+// // `.find()`
+// const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const elemBiggerThan5 = myNums.find(num => num > 5);
+// console.log(elemBiggerThan5); // Outputs 6
+
+// // `.filter()`
+// const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arrBiggerThan5 = myNums.filter(num => num > 5);
+// console.log(arrBiggerThan5); // Outputs [6, 7, 8, 9, 10]
+
+// // `.reduce()`
+// const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arrSum = myNums.reduce((runningSum, currNum) => runningSum + currNum, 0);
+// console.log(arrSum); // Outputs 55
+
+// `.concat()`
+// const myNums1 = [ 1, 2, 3];
+// const myNums2 = [ 4, 5, 6];
+// const combindedNums = myNums1.concat(myNums2, [7, 8]);
+// console.log(combindedNums); // Outputs [1, 2, 3, 4, 5, 6, 7, 8]
+
+// // `.slice()`
+// const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const mySlicedNums = myNums.slice(1, 4);
+// console.log(mySlicedNums); // Outputs [2, 3, 4]
+// myNums[1] = 10;
+// console.log(mySlicedNums); // Outputs [2, 3, 4] (Primitive elements)
+
+// const myArrs = [[1, 2], [3, 4], [5, 6]];
+// const mySlicedArrs = myArrs.slice(0, 2);
+// console.log(mySlicedArrs); // Outputs [[1, 2], [3, 4]]
+// myArrs[0][0] = 7;
+// console.log(mySlicedArrs); // Outputs [[7, 2], [3, 4]] (Reference elements)
+
+// // `splice()`
+// const myNums = [1, 4, 5];
+// // Adding elements at index 1
+// myNums.splice(1, 0, 2, 3); // Start at index 1, replace next 0 elements, with [2, 3]
+// console.log(myNums); // Outpus [1, 2, 3, 4, 5]
+
+// // Removing elements 
+// myNums.splice(3, 2); // Start at index 3, replace next 2 elements, with []
+// console.log(myNums); // Outpus [1, 2, 3]
+
+// // Replacing elements 
+// myNums.splice(1, 2, 4, 5); // Start at index 1, replace next 2 elements, with [4, 5]
+// console.log(myNums); // Outpus [1, 4, 5]
+
+// ------------- Destructuring ------------
+
+// const fullName = ["Coen", "Potgieter"];
+// const fName = fullName[0];
+// const lName = fullName[1];
+// console.log(fName);
+// console.log(lName);
+
+// const fullName = ["Coen", "Potgieter"];
+// const [fName, lName] = fullName;
+// console.log(fName);
+// console.log(lName);
+
+// const [fName, lName] = ["Coen", "Potgieter"];
+// console.log(fName);
+// console.log(lName);
+
+// const user = {
+//     userName: "Coen",
+//     userAge: 21
+// }
+// const { userName, userAge } = user;
+// console.log(userName);
+// console.log(userAge);
+
+// const { userName, userAge }= {
+//     userName: "Coen",
+//     userAge: 21
+// }
+// console.log(userName);
+// console.log(userAge);
+
+// const { userName: nameAlias, userAge }= {
+//     userName: "Coen",
+//     userAge: 21
+// }
+// console.log(nameAlias);
+// console.log(userAge);
+
+
+// ------------- Spread Operator ------------
+
+
+// // Combining Arrays
+// const arr1 = ["Hiya", "World"];
+// const arr2 = ["Extra Word"];
+// const combinedArr = [...arr1, ...arr2];
+// console.log(combinedArr);
+
+// // Combining Objects
+// const obj1 = { userName: "Coen12", userPassword: "strongPassword" };
+// const obj2 = { age: 21 };
+// const combinedObj = { ...obj1, ...obj2 };
+// console.log(combinedObj);
+
+// // Function Calls
+// function add3Nums(a, b, c) {
+//     return a + b + c;
+// }
+// const myNums = [1,2,3];
+// console.log(add3Nums(...myNums));
+
+// // Spread String into array of chars
+// const myString = "Hello World";
+// const myStringArr = [...myString];
+// console.log(myStringArr);
+
+// ------------- Statements ------------
+
+// If Statements
+// const inpPassword = prompt("Fill in Your Password");
+// if (inpPassword === "password12") {
+//     console.log("Access Granted");
+// } else if (inpPassword === "Password12") {
+//     console.log("Access Granted");
+// } else {
+//     console.log("Access Denied");
+// }
+
+// // For Loops (Normal)
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// // For Loops (Range-Based)
+// const myArr = ["Hello", "World", "Extra", "Words With Space"];
+// for (const elem of myArr) {
+//     console.log(elem);
+// }
+
+
+// ------------- Misc Things ------------
+
+// // Passing Functions
+// function handleTimeOut(func) {
+//     // Wait x amount of time
+//     func();
+// }
+// function notifyTimeUp() {
+//     console.log("Time is up!");
+// }
+// const notifyTimeUpVar = () => {
+//     console.log("Time is up!");
+// }
+
+// // Pass in function with no parenthesis
+// handleTimeOut(notifyTimeUp);
+// // Pass in function stored as var like a normal var
+// handleTimeOut(notifyTimeUpVar);
+// // Alternatively we could define the function in the parameter list
+// handleTimeOut(() => { console.log("Time is up!"); });
+
+// Nested Functions
+// function init() {
+//     function greet() {
+//         console.log("Hiya");
+//     }
+//     greet();
+// }
+// init();
+
+// // Can't call `greet()` from outside the `init()` function
+// // greet(); // Error
 
 
 
-
-// let mutableNumbers = [10, 23, 54];
-
-
-// hobbies[1] = "Port";
-// console.log(hobbies[1]);
-
-// mutableNumbers[0] = 11;
-// console.log(mutableNumbers[0]);
-
-// let expArray = [1, "HIYA"];
-// console.log(expArray[0]);
-// console.log(expArray[1]);
 
 
 
