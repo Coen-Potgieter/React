378b57cd84791893aa53c9b6aa4bf5f5260599cc
Gerhardus Coenraad Potgieter <25999656@sun.ac.za>
2024-12-07 09:45:15 +0200

    Learnt about props, will explore more concise syntax later

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,25 +1,60 @@
+import reactImg from "./assets/react-core-concepts.png";
+import compImg from "./assets/components.png";
+
+const startingWords = ["Fundamental", "Crucial", "Core"];
+
+function getRandInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 function MyFirstComp() {
+    // Defining dynamic content into a separate variable here
+    const randomDescr = startingWords[getRandInt(startingWords.length - 1)];
+
+    // Inside returned JSX code we inject this dynamic content using our `randomDescr` variable
     return (
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+            {randomDescr} React concepts you will need for almost any app you are
+            going to build!
         </p>
       </header>
     );
 }
 
+// Declaration of props component
+function CoreConceptsItem(props) {
+    return (
+    <li>
+        <img src={props.inpImg} alt={`${props.inpTitle}} Image`} />
+        <h3>{props.inpTitle}</h3>
+        <p>{props.inpDescr}</p>
+    </li>
+);
+}
+
 function App() {
-  return (
-    <div>
-      <MyFirstComp/>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <MyFirstComp/>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        {/* Inserting data into props component */}
+                        <CoreConceptsItem
+                            inpImg={compImg}
+                            inpTitle="Components"
+                            inpDescr="The Core UI building block"
+                        />
+                    </ul>
+                </section>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
