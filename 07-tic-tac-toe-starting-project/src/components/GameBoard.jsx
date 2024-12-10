9bc053018d7ce50a0624fadf703c141e35cfdb9e
Gerhardus Coenraad Potgieter <25999656@sun.ac.za>
2024-12-10 12:59:40 +0200

    Learnt about dynamically rendering 2D arrays, best practice when updating reference state values, will learn how to lift states next

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [ 
     [null, null, null],
     [null, null, null],
@@ -5,11 +7,34 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+    const [ isPlayer1Turn, setIsPlayer1Turn ] = useState(true);
+
+    function handleSquareSelect(rowIdx, colIdx) {
+        setIsPlayer1Turn((prevTurn) => !prevTurn);
+        setGameBoard((prevGameBoard) => {
+            const newGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+
+            newGameBoard[rowIdx][colIdx] = (isPlayer1Turn) ? "X" : "O";
+            return newGameBoard;
+        });
+    }
+    
     return (
         <ol id="game-board">
-            {initialGameBoard.map(symbol => (
-            <button>{symbol}</button>
+            {gameBoard.map((row, rowIdx) => (
+            <li key={rowIdx}>
+                    <ol>
+                        {row.map((symbol, colIdx) => (
+                            <li key={colIdx}>
+                                <button onClick={() => handleSquareSelect(rowIdx, colIdx)}>
+                                    {symbol}
+                                </button>
+                            </li>
+                        ))}
+                    </ol>
+            </li>
             ))}
         </ol>
-    )
+    );
 }
